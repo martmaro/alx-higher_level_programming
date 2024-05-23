110bea202181bad24d94f25c4c721e61f761c115
martmaro <odhisluka@gmail.com>
2024-05-22 17:36:52 -0700

    Status code

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,25 +1,12 @@
 #!/usr/bin/node
+// Status Of Get request
 
 const request = require('request');
-const id = process.argv[2];
-const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
-
-request.get(url, (error, response, body) => {
+const url = process.argv[2];
+request.get(url, function (error, response) {
   if (error) {
     console.log(error);
   } else {
-    const content = JSON.parse(body);
-    const characters = content.characters;
-    // console.log(characters);
-    for (const character of characters) {
-      request.get(character, (error, response, body) => {
-        if (error) {
-          console.log(error);
-        } else {
-          const names = JSON.parse(body);
-          console.log(names.name);
-        }
-      });
-    }
+    console.log('code:' + ' ' + response.statusCode);
   }
 });
